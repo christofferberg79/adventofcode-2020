67b651990177a1d5fc45d6f7149bd9c5c1693f53
Christoffer Berg <christoffer.berg@collectum.se>
2021-12-13 14:53:08 +0100

    template files

---
+++ src/main/kotlin/DayXX.kt
@@ -0,0 +1,9 @@
+package cberg.aoc2020
+
+class DayXX(private val input: List<String>) {
+    constructor() : this(Input("XX.txt").lines())
+
+    fun part1() = 0
+
+    fun part2() = 0
+}
---
+++ src/test/kotlin/DayXXTest.kt
@@ -0,0 +1,34 @@
+package cberg.aoc2020
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class DayXXTest {
+    private val exampleInput = listOf(
+        ""
+    )
+
+    @Test
+    fun part1_Example() {
+        val result = DayXX(exampleInput).part1()
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun part1_RealInput() {
+        val result = DayXX().part1()
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun part2_Example() {
+        val result = DayXX(exampleInput).part2()
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun part2_RealInput() {
+        val result = DayXX().part2()
+        assertEquals(0, result)
+    }
+}
