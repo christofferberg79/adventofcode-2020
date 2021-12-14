d60aaa51af73bfc3cd79454a4201cded62d30d89
Christoffer Berg <christoffer.berg@collectum.se>
2021-12-14 22:23:07 +0100

    day 7

---
+++ src/main/kotlin/Day07.kt
@@ -0,0 +1,35 @@
+package cberg.aoc2020
+
+class Day07(input: List<String>) {
+    constructor() : this(Input("07.txt").lines())
+
+    private val rules = parse(input)
+
+    fun part1() = rules.keys.count { bag -> bag.eventuallyContains("shiny gold") }
+
+    fun part2() = numberOfBagsIn("shiny gold")
+
+    private fun parse(input: List<String>) = input.associate { line ->
+        val parts = line.split("contain", ",").map { it.trim() }
+        val containingBag = parts[0].substringBefore(" bag")
+        val containedBags = parts.drop(1)
+            .filter { part -> part.first().isDigit() }
+            .associate { part ->
+                val bag = part.substringAfter(" ").substringBefore(" bag")
+                val num = part.substringBefore(" ").toInt()
+                bag to num
+            }
+
+        containingBag to containedBags
+    }
+
+    private fun String.eventuallyContains(otherBag: String): Boolean {
+        val containedBags = rules[this] ?: return false
+        return containedBags.keys.any { bag -> bag == otherBag || bag.eventuallyContains(otherBag) }
+    }
+
+    private fun numberOfBagsIn(bag: String): Int {
+        val containedBags = rules[bag] ?: return 0
+        return containedBags.entries.sumOf { (bag, num) -> num + num * numberOfBagsIn(bag) }
+    }
+}
---
+++ src/test/kotlin/Day07Test.kt
@@ -0,0 +1,42 @@
+package cberg.aoc2020
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day07Test {
+    private val exampleInput = listOf(
+        "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+        "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+        "bright white bags contain 1 shiny gold bag.",
+        "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+        "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+        "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+        "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+        "faded blue bags contain no other bags.",
+        "dotted black bags contain no other bags."
+    )
+
+    @Test
+    fun part1_Example() {
+        val result = Day07(exampleInput).part1()
+        assertEquals(4, result)
+    }
+
+    @Test
+    fun part1_RealInput() {
+        val result = Day07().part1()
+        assertEquals(355, result)
+    }
+
+    @Test
+    fun part2_Example() {
+        val result = Day07(exampleInput).part2()
+        assertEquals(32, result)
+    }
+
+    @Test
+    fun part2_RealInput() {
+        val result = Day07().part2()
+        assertEquals(5312, result)
+    }
+}
