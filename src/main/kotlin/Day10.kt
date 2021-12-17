5262243da57cf5b6e253574eb02b164543718c79
Christoffer Berg <christoffer.berg@collectum.se>
2021-12-17 16:15:01 +0100

    day 10

---
+++ src/main/kotlin/Day10.kt
@@ -0,0 +1,18 @@
+package cberg.aoc2020
+
+class Day10(input: List<Int>) {
+    constructor() : this(Input("10.txt").intLines())
+
+    private val max = input.maxOrNull()!!
+    private val adapters = (input + 0 + (max + 3)).toSet()
+    private val numArrangements = mutableMapOf(max to 1L)
+
+    fun part1() = adapters.sorted().zipWithNext { a, b -> b - a }
+        .let { diffs -> diffs.count { it == 1 } * diffs.count { it == 3 } }
+
+    fun part2() = getArrangementsFrom(0)
+
+    private fun getArrangementsFrom(n: Int): Long = numArrangements.getOrPut(n) {
+        (n + 1..n + 3).filter { it in adapters }.sumOf { getArrangementsFrom(it) }
+    }
+}
---
+++ src/test/kotlin/Day10Test.kt
@@ -0,0 +1,32 @@
+package cberg.aoc2020
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day10Test {
+    private val exampleInput = listOf(16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4)
+
+    @Test
+    fun part1_Example() {
+        val result = Day10(exampleInput).part1()
+        assertEquals(35, result)
+    }
+
+    @Test
+    fun part1_RealInput() {
+        val result = Day10().part1()
+        assertEquals(2812, result)
+    }
+
+    @Test
+    fun part2_Example() {
+        val result = Day10(exampleInput).part2()
+        assertEquals(8, result)
+    }
+
+    @Test
+    fun part2_RealInput() {
+        val result = Day10().part2()
+        assertEquals(386869246296064, result)
+    }
+}
