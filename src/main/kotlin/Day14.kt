43a99b8590a16f28f25e25a95022800e1a3b5abc
Christoffer Berg <christoffer.berg@collectum.se>
2021-12-19 13:03:43 +0100

    day 14

---
+++ src/main/kotlin/Day14.kt
@@ -0,0 +1,43 @@
+package cberg.aoc2020
+
+class Day14(private val input: List<String>) {
+    constructor() : this(Input("14.txt").lines())
+
+    fun part1() = runProgram { memory, mask, addr, value ->
+        val binary = value.toString(2).padStart(36, '0')
+        val masked = binary.mapIndexed { i, c -> if (mask[i] == 'X') c else mask[i] }.joinToString("")
+        memory[addr] = masked.toLong(2)
+    }
+
+    fun part2() = runProgram { memory, mask, addr, value ->
+        val binary = addr.toString(2).padStart(36, '0')
+        val masked = binary.mapIndexed { i, c -> if (mask[i] == '0') c else mask[i] }.joinToString("")
+
+        for (a in unfloat(masked)) {
+            memory[a.toLong(2)] = value
+        }
+    }
+
+    private fun unfloat(s: String): List<String> = when (val i = s.indexOf('X')) {
+        -1 -> listOf(s)
+        else -> unfloat(s.replaceRange(i, i + 1, "0")) +
+                unfloat(s.replaceRange(i, i + 1, "1"))
+    }
+
+    private fun runProgram(decoder: (MutableMap<Long, Long>, String, Long, Long) -> Unit): Long {
+        val memory = mutableMapOf<Long, Long>()
+        var mask = ""
+        for (line in input) {
+            if (line.startsWith("mask")) {
+                mask = line.substringAfter("mask = ")
+            } else {
+                val (addr, value) = Regex("mem\\[(.*)] = (.*)").matchEntire(line)
+                    ?.destructured?.let { (s1, s2) -> s1.toLong() to s2.toLong() }
+                    ?: error("Invalid input: $line")
+                decoder(memory, mask, addr, value)
+            }
+        }
+        return memory.values.sum()
+    }
+
+}
---
+++ src/test/kotlin/Day14Test.kt
@@ -0,0 +1,44 @@
+package cberg.aoc2020
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day14Test {
+    private val exampleInput1 = listOf(
+        "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
+        "mem[8] = 11",
+        "mem[7] = 101",
+        "mem[8] = 0"
+    )
+
+    private val exampleInput2 = listOf(
+        "mask = 000000000000000000000000000000X1001X",
+        "mem[42] = 100",
+        "mask = 00000000000000000000000000000000X0XX",
+        "mem[26] = 1"
+    )
+
+    @Test
+    fun part1_Example() {
+        val result = Day14(exampleInput1).part1()
+        assertEquals(165, result)
+    }
+
+    @Test
+    fun part1_RealInput() {
+        val result = Day14().part1()
+        assertEquals(13556564111697, result)
+    }
+
+    @Test
+    fun part2_Example() {
+        val result = Day14(exampleInput2).part2()
+        assertEquals(208, result)
+    }
+
+    @Test
+    fun part2_RealInput() {
+        val result = Day14().part2()
+        assertEquals(4173715962894, result)
+    }
+}
